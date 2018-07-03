b47119c6f23463054082ee4d684bcbe44fdad900
Aldrin Navarro <aldrin@channelfix.com>
2018-07-03 14:32:54 +0800

    Let's go!

---
+++ components/Feed.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="feed">
+		<div class="card">
+			<div class="card-header">
+				<div class="avatar"></div>
+				<div class="name">Mememe</div>
+			</div>
+			<div class="card-body"></div>
+			<div class="card-footer">
+				<div class="actions"></div>
+				<div class="actions actions-right"></div>
+			</div>
+		</div>
+		<div class="card"></div>
+		<div class="card"></div>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style scoped>
+.feed {
+	height: 100%;
+	padding-top: 40px;
+}
+
+.card {
+	border: 1px solid black;
+	margin-bottom: 60px;
+}
+
+.card-header {
+	border: 1px solid black;
+	height: 60px;
+	width: 100%;
+	padding: 10px;
+	display: inline-block;	
+}
+
+.card-header > div {
+	float: left;
+}
+
+.card-header .avatar {
+	background-color: black;
+	border-radius: 20px;
+	height: 40px;
+	width: 40px;
+}
+
+.card-header .name {
+	margin-left: 10px;
+}
+
+.card-body {
+	width: 614px;
+	height: 614px;
+}
+
+.card-footer {
+	border: 0.5px solid gray;
+	width: 100%;
+	min-height: 40px;
+	display: inline-block;
+}
+
+.card-footer .actions {
+	border: 0.25px solid gray;
+	width: 40px;
+	height: 40px;
+	float: left;
+}
+
+.card-footer .actions.actions-right {
+	float: right;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <nav></nav>
+    <div class="feed-wrapper">
+      <Feed></Feed>
+    </div>
+  </div>
+</template>
+
+<script>
+import Feed from '~/components/Feed.vue'
+
+export default {
+  components: {
+    Feed
+  }
+}
+</script>
+
+<style>
+
+html {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  letter-spacing: 1px;
+}
+
+html, body, .container {
+  margin: 0 auto;
+  padding: 0;
+  min-height: 100vh;
+}
+
+nav {
+  height: 50px;
+  width: 100%;
+  border-bottom: 1px solid gray;
+}
+
+.feed-wrapper {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
